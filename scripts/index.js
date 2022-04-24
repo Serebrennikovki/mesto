f205dc921e6e755ddd12321ff2eef80c265b6abc
Serebrennikov <serebrennikov_k_i@mail.ru>
2022-04-24 22:37:17 +0500

    feat: change img and name in card__table by js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,54 @@
  const popupElement = document.querySelector('.popup');
  const editButton = document.querySelector('.profile__change-button');
  const closeButton = document.querySelector('.popup__close-button');
+ const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }];
 
  let formElement = document.querySelector('.popup__window');
  let nameInput = document.querySelector('.popup__input-text_field_name');
  let jobInput = document.querySelector('.popup__input-text_field_job');
  let nameField =  document.querySelector('.profile__name');
  let jobField = document.querySelector('.profile__job');
+ const cardsTable = document.querySelector('.cards__table');
+
+ function onLoadWindow(){
+     console.log('hi');
+     console.log(cardsTable);
+     const cardArray = document.querySelectorAll('.card');
+     console.log(cardArray);
+     for (let i = 0; i<cardArray.length; i++ ){
+      let cardIMG = cardArray[i].querySelector('.card__image');
+      console.log(cardIMG);
+      cardIMG.src = initialCards[i].link;
+      let cardName = cardArray[i].querySelector('.card__name');
+      console.log(cardName);
+      cardName.textContent = initialCards[i].name;
+     }
+    
+     }
+
 
 
  function openPopup(){
@@ -38,8 +80,10 @@
     closePopup();
 }
    
-
+ document.addEventListener("DOMContentLoaded", onLoadWindow());
  editButton.addEventListener('click', openPopup);
 
  closeButton.addEventListener('click', closePopup);
  formElement.addEventListener('submit', formSubmitHandler);
+
+ 
